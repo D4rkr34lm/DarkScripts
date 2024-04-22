bfee3da2b02fd4e95f4416da2d5f888b0fd730e5
D4rkr34lm <darth.m.frohn@gmail.com>
2024-04-22 22:03:29 +0200

    fixed core coloring

--- core.lua
+++ core.lua
@@ -3,12 +3,12 @@ Core = {}
 ---Sends a prefixed message
 ---@param message string
 function Core.send(message)
-  print("§f[§8Dark§3Scripts§f]§7 ", message)
+  print("&f[&8Dark&3Scripts&f]&7 ", message)
 end
 
 ---Highlights the given message
 ---@param text string
 ---@return string
 function Core.highlight(text)
-  return "§9" .. text .. "§7"
+  return "&9" .. text .. "&7"
 end
