090920e1fd2b3dcfb64039076544cfb7ee0365a2
D4rkr34lm <darth.m.frohn@gmail.com>
2024-04-22 21:31:39 +0200

    Added tps module Added highlight to core

--- core.lua
+++ core.lua
@@ -1,6 +1,14 @@
 Core = {}
 
+---Sends a prefixed message
 ---@param message string
 function Core.send(message)
-  print("§f[§8Dark§3Scripts§f]", message)
+  print("§f[§8Dark§3Scripts§f]§7 ", message)
+end
+
+---Highlights the given message
+---@param text string
+---@return string
+function Core.highlight(text)
+  return "§9" .. text .. "§7"
 end
---
+++ modules/TPS.lua
@@ -0,0 +1,27 @@
+TPS = {}
+
+StepSize = 1
+
+function TPS.handleStepSizeCommand(params)
+  if params[1] == nil then
+    Core.send("StepSize is " .. Core.highlight(tostring(StepSize)))
+  else
+    Core.send("Set StepSize to " .. Core.highlight(params[1]))
+  end
+end
+
+function TPS.handleTickStep(pressed)
+  if pressed then
+    exec("tick step " .. tostring(StepSize))
+  end
+end
+
+function TPS.handleTickUnfreeze(pressed)
+  if pressed then
+    exec("tpslimit 20")
+  end
+end
+
+hotkey("x", TPS.handleTickStep)
+hotkey("ctrl+x", TPS.handleTickUnfreeze)
+command("stepsize", TPS.handleStepSizeCommand)
